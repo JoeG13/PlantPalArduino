72815bec69fa743fc990cacca1a0e23d25c5b505
JoeG13 <josephglogowski@icloud.com>
2024-04-08 10:47:10 -0300

    Finalized PlantPal Arduino Code
    Final version of tech fair code

--- PlantPalArduinoCode/PlantPalArduinoCode.ino
+++ PlantPalArduinoCode/PlantPalArduinoCode.ino
@@ -1,27 +1,35 @@
-#include "DHT.h"
+/*
+  Author: Joe Glogowski
+  Project: Plant Pal
+  Purpose: This project is for the Plant Pal project at the Tech Showcase.
+  Date: April 5, 2024
+*/
 
-const int soilAnalog = A0;
-const int waterAnalog = A1;
-const int tempMoistureAnalog = A2;
-const int lightAnalog = A3;
-const int pumpPin = 8;
+#include "DHT.h"; // Include DHT library.
 
-int soilMoistureLevel = 0;
-int waterLevel = 0;
-int temperatureLevel = 0;
-int humidityLevel = 0;
-int lightLevel = 0;
+const int soilAnalog = A0; // Soil moisture analog pin.
+const int waterAnalog = A1; // Water level analog pin.
+const int tempMoistureAnalog = A2; // Temperature and moisture analog pin.
+const int lightAnalog = A3; // Light analog pin.
+const int pumpPin = 8; // Pump digital pin.
 
-// Create the DHT object
+int soilMoistureLevel = 0; // Soil moisture level.
+int waterLevel = 0; // Water level.
+int temperatureLevel = 0; // Temperature level.
+int humidityLevel = 0; // Humidity level.
+int lightLevel = 0; // Light level.
+
+// DHT object for temperature and humidity sensor.
 DHT dht(tempMoistureAnalog, DHT11);
 
 void setup() {
   Serial.begin(9600);
 
   dht.begin();
-  pinMode(pumpPin, OUTPUT);
+  pinMode(pumpPin, OUTPUT); // Set pump pin mode to output.
 }
 
+// Read sensor values.
 void readSensorValues() {
   soilMoistureLevel = analogRead(soilAnalog);
   waterLevel = analogRead(waterAnalog);
@@ -30,35 +38,41 @@ void readSensorValues() {
   lightLevel = analogRead(lightAnalog);
 }
 
+// Send sensor data.
 void sendSensorData() {
-  Serial.println("Sensor Data:");
   Serial.print("Soil Moisture: ");
   Serial.println(soilMoistureLevel);
   Serial.print("Water Level: ");
   Serial.println(waterLevel);
   Serial.print("Temperature: ");
-  Serial.println(temperatureLevel);
-  Serial.print("Humidity: ");
+  Serial.print(temperatureLevel);
+  Serial.print(" degrees celsius");
+  Serial.print("\nHumidity: ");
   Serial.println(humidityLevel);
   Serial.print("Light: ");
   Serial.println(lightLevel);
 }
 
+// Control the pump based on soil moisture level.
 void controlPump() {
-  if (soilMoistureLevel < 500) {  // Adjust the threshold as needed
-    digitalWrite(pumpPin, HIGH);
+  if (soilMoistureLevel < 500) {
+    digitalWrite(pumpPin, HIGH); // Turn on pump.
     Serial.println("\nPump turned on.");
-    delay(1000);  // Run the pump for 5 seconds (adjust as needed)
-    digitalWrite(pumpPin, LOW);
+    delay(1000); // Run pump for 5 seconds.
+    digitalWrite(pumpPin, LOW); // Turn off pump.
     Serial.println("\nPump turned off.");
   } else {
     Serial.println("\nPump off");
   }
 }
 
+
+
+
 void loop() {
   readSensorValues();
   sendSensorData();
   controlPump();
-  delay(10000);  // Adjust delay as needed
+  delay(10000); // Delay between sensor readings.
+  Serial.println("\n\n\n");
 }
